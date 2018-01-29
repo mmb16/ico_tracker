3518919fe40f97e6a01831eab25a6e11c4b47a49
mmb16 <mmb583@nyu.edu>
2018-01-28 19:59:13 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,41 +21,4 @@ class ApplicationController < Sinatra::Base
    end
  end
 
-  get '/signup' do
-    if logged_in?
-      redirect to '/icos'
-    else
-      erb :'/users/create_user'
-    end
-  end
-
-  post '/signup' do
-    if params["username"] != "" && params["password"] != ""
-      @user = User.create(params)
-      @user.save
-      session[:user_id] = @user.id
-      redirect to '/icos'
-    end
-    redirect to '/signup'
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect to '/icos'
-    else
-      erb :'/users/login'
-    end
-  end
-
-  post '/login' do
-    @user = User.find_by(username: params[:username])
-    session[:user_id] = @user.id
-    redirect to '/icos'
-  end
-
-  get '/logout' do
-    session.clear
-    redirect to '/login'
-  end
-
 end
